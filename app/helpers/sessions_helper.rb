1adcea83f6c8d5745493bb5ecb82d6d87eea7db3
horisaki <seiji.horisaki@gmail.com>
2018-06-13 18:52:29 +0900

    twitter認証

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: 'ログインしてください'
+  end
+end
